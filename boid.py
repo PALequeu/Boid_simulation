ee43d63af43c2b9bb43feed57af62bb2c99d34ba
PALequeu <pierreantoinelequeu@gmail.com>
2023-06-27 09:40:40 +0200

    functionnal flocking for boids

--- boid.py
+++ boid.py
@@ -1,82 +1,83 @@
-from p5 import *
+from p5 import Vector
 import numpy as np
 import random as rd
 
+import pygame
+
 class Boid():
 
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height, map, leader = None):
         self.position = Vector(x, y)
         vec = (np.random.rand(2) - 0.5)*10
-        self.velocity = Vector(*vec)
+        #self.velocity = Vector(*vec)
+        self.velocity = Vector(0.1,0.1)
 
         vec = (np.random.rand(2) - 0.5)/2
         self.acceleration = Vector(*vec)
         self.max_force = 0.5
-        self.max_speed = 5
-        self.perception = 50
+        self.max_speed = 2
+        self.perception = 99
+        
+        self.leader = leader
 
         self.width = width
         self.height = height
-    
+        
+        self.map = map
 
 
     def update(self,mode):
+        distance = np.linalg.norm(self.position - self.leader.position)
+        print(distance)
         if mode == "boids":
             self.position += self.velocity
-            self.velocity += self.acceleration
+            velocity = self.velocity+self.acceleration
+            if distance > 100:
+                velocity = velocity*float(np.exp(-(10/(distance-100))))
+            if distance == 100:
+                velocity = 0
+            #if distance < 100:
+            #    velocity = velocity*float(np.exp(-(10/(distance))))
+
+            self.velocity = velocity
+            print(self.velocity)
             #limit
             if np.linalg.norm(self.velocity) > self.max_speed:
                 self.velocity = self.velocity / np.linalg.norm(self.velocity) * self.max_speed
 
             self.acceleration = Vector(*np.zeros(2))
-        if mode == "carré" : 
-            #comportement du master
-            x,y = self.position.x,self.position.y
-            if x > 950: 
-                self.acceleration.x = -1
-            if x < 50:
-                self.acceleration.x = 1
-            if y > 950:
-                self.acceleration.y = -1
-            if y < 50: 
-                self.acceleration.y = 1
-            if np.linalg.norm(self.acceleration) > self.max_force:
-                self.acceleration = (self.acceleration /np.linalg.norm(self.acceleration)) * self.max_force            
-            self.position += self.velocity
-            self.velocity += self.acceleration
-            #limit
-            if np.linalg.norm(self.velocity) > self.max_speed:
-                self.velocity = self.velocity / np.linalg.norm(self.velocity) * self.max_speed
-
-            self.acceleration = Vector(*np.zeros(2))
-    def show(self,count,boids):
-        stroke(255)
-        fill(255)
-        circle((self.position.x, self.position.y), radius=10)
-        if (True):#count == 1):
-            align = self.align(boids)
-            cohes = self.cohesion(boids)
-            sep = self.separation(boids)
-            att = self.attraction(boids)
-            self.drawArrow(Color(255,0,0),align)
-            self.drawArrow(Color(0,0,255),cohes)
-            self.drawArrow(Color(0,255,0),sep)
-            self.drawArrow(Color(100,100,100),att)
-    def leadershow(self):
-        stroke(Color(255,0,0))
-        fill(Color(0,255,0))
-        circle((self.position.x, self.position.y), radius=10)
+            
+    
+            
+    def show(self,boids, barycenter, leader = False):
+        boid_pos = pygame.Vector2(self.position.x, self.position.y)
+        if(not leader):
+            pygame.draw.circle(self.map, "red", boid_pos, 10)
+            #pygame.draw.circle(self.map, "red", boid_pos, 100, 1)
+            
+        else :
+            pygame.draw.circle(self.map, "blue", boid_pos, 10)
+            #pygame.draw.circle(self.map, "blue", boid_pos, 100, 1)
+        #if self.leader != None :
+            #cohes = self.cohesion()
+            #align = self.align(boids)
+            #self.drawArrow("red",align)
+            #self.drawArrow("blue",cohes)
+            
+        
+        #sep = self.separation(boids)
+        #self.drawArrow("green",sep)
+ 
 
 
     def apply_behaviour(self, boids):
-        alignment = self.align(boids)
-        cohesion = self.cohesion(boids)
+        if self.leader != None :
+            #alignment = self.align(boids)
+            cohesion = self.cohesion()
+            #self.acceleration += alignment
+            self.acceleration += cohesion
+       
         separation = self.separation(boids)
-        attraction = self.attraction(boids)
-
-        self.acceleration += attraction
-        self.acceleration += alignment
-        self.acceleration += cohesion
         self.acceleration += separation
 
     def edges(self):
@@ -108,73 +109,91 @@ class Boid():
 
         return steering
 
-    def cohesion(self, boids):
-        steering = Vector(*np.zeros(2))
-        total = 0
-        center_of_mass = Vector(*np.zeros(2))
-        i=0
-        green_close = False
-        for boid in boids:
-           i+=1
-           if np.linalg.norm(boid.position - self.position) < self.perception:
-                if i == 1:
-                    green_close=True
-                    for j in range (0,19):
-                        center_of_mass += boid.position
-                        total+=20
-                center_of_mass += boid.position
-                total += 1
-        if total > 0:
-            center_of_mass /= total
-            center_of_mass = Vector(*center_of_mass)
-            vec_to_com = center_of_mass - self.position
-            if np.linalg.norm(vec_to_com) > 0:
-                vec_to_com = (vec_to_com / np.linalg.norm(vec_to_com)) * self.max_speed
-            steering = vec_to_com - self.velocity
-            if np.linalg.norm(steering)> self.max_force:
-                steering = (steering /np.linalg.norm(steering)) * self.max_force*(0.3+0.7*green_close)
-
+    def cohesion(self):
+        # steering = Vector(*np.zeros(2))
+        # total = 0
+        # center_of_mass = Vector(*np.zeros(2))
+        # i=0
+        # green_close = False
+        # for boid in boids:
+        #     i+=1
+        #     if np.linalg.norm(boid.position - self.position) < self.perception:
+        #         if i == 1:
+        #             green_close=True
+        #             for j in range (0,19):
+        #                 center_of_mass += boid.position
+        #                 total+=20
+        #         center_of_mass += boid.position
+        #         total += 1
+        
+        # if total > 0:
+        #     center_of_mass /= total
+        #     center_of_mass = Vector(*center_of_mass)
+        # print("barycenter",barycenter,"self.position",self.position)
+        # barycenter = Vector(barycenter.x, barycenter.y)
+       
+      
+        vec_to_com = self.leader.position - self.position
+        if np.linalg.norm(vec_to_com) > 0:
+            vec_to_com = (vec_to_com / np.linalg.norm(vec_to_com)) * self.max_speed
+        steering = vec_to_com - self.velocity
+        if np.linalg.norm(steering)> self.max_force:
+            steering = (steering /np.linalg.norm(steering)) * self.max_force
+        #steering = steering*int(distance)
+        #print("cohesion", steering)
+        distance = np.linalg.norm(self.position - self.leader.position)
+        if distance > 100:
+            steering *= float(np.exp(-(10/(distance-100))))
+        else :
+            steering = -steering
         return steering
+    
 
     def separation(self, boids):
         steering = Vector(*np.zeros(2))
         total = 0
         avg_vector = Vector(*np.zeros(2))
         i=0
+        
+        #for boids
         for boid in boids:
             distance = np.linalg.norm(boid.position - self.position)
             if self.position != boid.position and distance < self.perception:
-                diff = self.position - boid.position
-                diff /= distance
+                diff = (self.position - boid.position) * float(np.exp((10/(distance))))
+                #diff /= distance
                 avg_vector += diff*(self.perception-distance)
                 total += 1
+        #for leader
+        if self.leader != None:
+            distance = np.linalg.norm(self.leader.position - self.position)
+            if self.position != self.leader.position and distance < self.perception:
+                diff = self.position - self.leader.position
+                diff *= float(np.exp((10/(distance))))
+                avg_vector += diff*(self.perception-distance)
+                total += 1
+                
         if total > 0:
             avg_vector /= total
             avg_vector = Vector(*avg_vector)
             if np.linalg.norm(steering) > 0:
                 avg_vector = (avg_vector / np.linalg.norm(steering)) * self.max_speed
             steering = avg_vector - self.velocity
-            if np.linalg.norm(steering) > self.max_force:
-                steering = (steering /np.linalg.norm(steering)) * self.max_force
+            if np.linalg.norm(steering) > 1*self.max_force:
+                steering = (steering /np.linalg.norm(steering)) *1*self.max_force
+        #print("separation", steering)
         return steering
-    def attraction(self, boids): 
-        #force d'attraction au master, qui change assez peu le comportement du flock
-        steering = Vector(*np.zeros(2))
-        boid = boids[0]
-        distance = np.linalg.norm(boid.position - self.position)
-        if self.position != boid.position and distance < self.perception:
-            diff = self.position - boid.position
-            diff /= distance
-            if np.linalg.norm(steering) > 0:
-                diff = (diff / np.linalg.norm(diff)) * self.max_speed
-            steering = diff - self.velocity
-            if np.linalg.norm(steering) > self.max_force:
-                steering = (steering /np.linalg.norm(steering)) * self.max_force
-        return steering
-    def drawArrow(self,my_color,vect):
-            stroke(my_color)
-            fill(my_color)
-            line(self.position,vect*100+self.position)
+
+
+
+    
+    def drawArrow(self,color,vect):
+            effect = pygame.Vector2(vect.x, vect.y)
+            boid_pos = pygame.Vector2(self.position.x, self.position.y)
+            pygame.draw.line(self.map,color,boid_pos,effect*100+boid_pos)
+            # pygame.stroke(my_color)
+            # fill(my_color)
+            # line(self.position,vect*100+self.position)
+    
     def Leader(self,mode):
         if (mode == "random"):
             self.acceleration= Vector(rd.uniform(-1,1),rd.uniform(-1,1))
--- main.py
+++ main.py
@@ -1,40 +1,99 @@
 from p5 import setup, draw, size, background, run, color, Vector
 import numpy as np
+import pygame
 from boid import Boid
+from flock import Flock
+
+# width = 1000
+# height = 1000
+
+# pygame.init()
+# font = pygame.font.Font("arial.ttf", 25)
+
+# def setup():
+#     #this happens just once
+#     size(width, height) #instead of create_canvas
 
 
-width = 1000
-height = 1000
+# def draw():
+#     global flock
+#     count = 0
+#     leadi= 0
+#     background(30, 30, 47)
 
-flock = [Boid(*np.random.rand(2)*1000, width, height) for _ in range(30)]
+#     for boid in flock:
+#         count+=1
+#         leadi+=1
+#         if (leadi == 1):
+#             boid.edges()
+#             boid.Leader("carré")
+#             boid.update("carré")
+#             boid.leadershow()
 
+#         else:
+#             boid.edges()
+#             boid.apply_behaviour(flock)
+#             boid.update("boids")
+#             boid.show(count,flock)
 
-def setup():
-    #this happens just once
-    size(width, height) #instead of create_canvas
+# #run(frame_rate=100)
+# #run(frame_rate=200)
+# run()
 
+class game() :
+    def __init__(self, numberOfBoids = 5, w=800, h=800 ):
+        self.w = w
+        self.h = h
+        self.start_ticks = pygame.time.get_ticks()
+        self.clock = pygame.time.Clock()
+        self.map = pygame.display.set_mode((self.w, self.h))
+        self.flock = Flock(numberOfBoids,w ,h, self.map) 
+        self.dt = 0
+        
+    def play(self):
+        running = True
+        player_pos = pygame.Vector2(self.map.get_width() / 2, self.map.get_height() / 2)
+        while running :
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            
+            self.map.fill("black")
+            
+            
+            #draw the leader
+            #self.flock.update_leader()
+            boid_pos = pygame.Vector2(self.flock.leader.position.x, self.flock.leader.position.y)
+            pygame.draw.circle(self.map, "blue", boid_pos, 10)
+            #pygame.draw.circle(self.map, "blue", boid_pos, 100, 1)
+            #self.flock.update_leader()
+            
+            #draw the boids
+            self.flock.update_boids()
+            
+            #draw the barycenter
+            #self.flock.draw_barycenter(self.map)
+            
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_z]:
+                self.flock.leader.position.y -= 100 * self.dt
+            if keys[pygame.K_s]:
+                self.flock.leader.position.y += 100 * self.dt
+            if keys[pygame.K_q]:
+                self.flock.leader.position.x -= 100 * self.dt
+            if keys[pygame.K_d]:
+                self.flock.leader.position.x += 100 * self.dt
+                
+            pygame.display.flip()
+            
+            self.dt = self.clock.tick(60) / 1000
+        pygame.quit()
+        
+    
 
-def draw():
-    global flock
-    count = 0
-    leadi= 0
-    background(30, 30, 47)
+if __name__ == "__main__":
+    game = game(10)
+    game.play()
 
-    for boid in flock:
-        count+=1
-        leadi+=1
-        if (leadi == 1):
-            boid.edges()
-            boid.Leader("carré")
-            boid.update("carré")
-            boid.leadershow()
-
-        else:
-            boid.edges()
-            boid.apply_behaviour(flock)
-            boid.update("boids")
-            boid.show(count,flock)
-
-#run(frame_rate=100)
-#run(frame_rate=200)
-run()
+                
+    
